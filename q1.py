7169cd920065f9d57752fb2a3f77aae69ab235f7
Novachrono117 <111282766+Novachrono117@users.noreply.github.com>
2022-12-05 09:05:32 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -22,8 +22,14 @@
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
 def q1(heights):
+    x = max(heights)
+    maior = 0
+    for item in heights:
+        if item == x:
+            maior += 1  
     
-    return heights.count(max(heights))
+    
+    return maior
 
 
 
